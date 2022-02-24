2eb3fd0aeb25070360d0c51593020b7747d5a895
hua <409801179@qq.com>
2022-02-24 17:10:24 +0800

    修正视频会议开始之后，新邀请的成员没有渲染画面的问题

--- iOS/Source/UI/TUIGroupCallingView.m
+++ iOS/Source/UI/TUIGroupCallingView.m
@@ -150,7 +150,7 @@
     [self addSubview:self.closeCameraBtn];
     [self addSubview:self.hangupBtn];
     [self addSubview:self.switchCameraBtn];
-    // 视图约束
+    
     [self.groupCollectionView mas_makeConstraints:^(MASConstraintMaker *make) {
         make.top.equalTo(self).offset(StatusBar_Height);
         make.centerX.equalTo(self);
@@ -287,17 +287,44 @@
     if (!user) return;
     
     NSInteger index = [self getIndexForUser:user];
-    if (index < 0) return;
+    BOOL isNewComingUser = (index<0);
+    
+    if (index < 0) {
+        [self.userList addObject:user];
+        index = self.userList.count - 1;
+        
+    } else {
+        self.userList[index] = user;
+    }
     
     self.curCallingState = TUICallingStateCalling;
     user.isEnter = YES;
-    self.userList[index] = user;
+    
     [self.delegateManager reloadCallingGroupWithModel:self.userList];
-    [self.delegateManager reloadGroupCellWithIndex:index];
+    
+    if (isNewComingUser) {
+        [self.delegateManager.collectionView reloadData];
+        
+    } else {
+        [self.delegateManager reloadGroupCellWithIndex:index];
+    }
     
     if (self.isVideo) {
-        UIView *renderView = [self.delegateManager getRenderViewFromUser:user.userId];
+        [self renderRemoteUser:user tryout:5];
+    }
+}
+
+- (void)renderRemoteUser:(CallUserModel*)user tryout:(NSInteger)times {
+    UIView *renderView = [self.delegateManager getRenderViewFromUser:user.userId];
+    
+    if (renderView) {
         [[TRTCCalling shareInstance] startRemoteView:user.userId view:renderView];
+        
+    } else if (times > 0) {
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW,(int64_t)300*NSEC_PER_MSEC), dispatch_get_main_queue(), ^{
+            
+            [self renderRemoteUser:user tryout:times-1];
+        });
     }
 }
 
@@ -369,7 +396,7 @@
     UIView *localRenderView = [self.delegateManager getRenderViewFromUser:self.currentUser.userId];
     
     if (!localRenderView) {
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(300 * NSEC_PER_MSEC)), dispatch_get_main_queue(), ^{
             [self handleLocalRenderView];
         });
         return;
@@ -602,5 +629,6 @@
     }
     return _calleeCollectionView;
 }
+
 
 @end
