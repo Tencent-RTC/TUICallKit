ec6d3bcee8880fd1f3e5e03c285b6eeb504f54c5
connor <48504520+jiushu531@users.noreply.github.com>
2022-05-16 19:44:33 +0800

    Revert "[MiniProgram]update report"

--- MiniProgram/app.js
+++ MiniProgram/app.js
@@ -1,6 +1,4 @@
 import { genTestUserSig } from './debug/GenerateTestUserSig'
-import Aegis from './lib/aegis'
-
 const Signature = genTestUserSig('')
 App({
   onLaunch: function() {
@@ -15,30 +13,6 @@ App({
       expiresIn: '',
       phone: '',
       sessionID: '',
-    }
-    this.aegisInit()
-    this.aegisReportEvent('onLaunch', 'onLaunch-success')
-  },
-  aegisInit() {
-    wx.aegis = new Aegis({
-      id: 'iHWefAYqxqlqtLQVcA', // 项目key
-      reportApiSpeed: true, // 接口测速
-      reportAssetSpeed: true, // 静态资源测速
-      pagePerformance: true, // 开启页面测速
-    });
-  },
-  aegisReportEvent(name, ext1) {
-    if (!this.aegisReportEvent[name] || !this.aegisReportEvent[name][ext1]) {
-      wx.aegis.reportEvent({
-        name,
-        ext1,
-        ext2: 'wxTUICallingExternal',
-        ext3: genTestUserSig('').sdkAppID,
-      });
-      if(typeof this.aegisReportEvent[name] !== 'object') {
-        this.aegisReportEvent[name] = {};
-      }
-      this.aegisReportEvent[name][ext1] = true;
     }
   },
 })
