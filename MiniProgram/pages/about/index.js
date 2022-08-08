0b8ae84cbb4f984456a3bf9bc61090ba41c2963f
yuanfazheng <yuanfazheng@tencent.com>
2022-08-08 14:58:22 +0800

    [MiniProgram]calling version updated to 1.0.0

--- MiniProgram/pages/about/index.js
+++ MiniProgram/pages/about/index.js
@@ -20,69 +20,69 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function() {
-    console.log(this.data.list)
-    console.log('获取当前版本', wx.getAccountInfoSync())
+  onLoad() {
+    console.log(this.data.list);
+    console.log('获取当前版本', wx.getAccountInfoSync());
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function() {
+  onReady() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function() {
+  onShow() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function() {
+  onHide() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function() {
+  onUnload() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function() {
+  onPullDownRefresh() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function() {
+  onReachBottom() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function() {
+  onShareAppMessage() {
 
   },
   onBack() {
     wx.navigateBack({
       delta: 1,
-    })
+    });
   },
   // 路由跳转
   handleRouter(e) {
-    const data = e.currentTarget.dataset.item
+    const data = e.currentTarget.dataset.item;
     if (data.path) {
-      wx.navigateTo({ url: `${data.path}` })
+      wx.navigateTo({ url: `${data.path}` });
     }
   },
-})
+});
