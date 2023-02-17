07b4e117152f9a74f76b4ba1e240e459d1eb1205
rgzhao <rgzhao@tencent.com>
2023-02-18 00:15:30 +0800

    feat: optimize uni-app/TUICallKit-Miniprogram demo and readme

---
+++ uni-app/TUICallKit-Miniprogram/TUICallKit-Vue3/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import TIM from 'tim-wx-sdk';
+import { genTestUserSig }  from  './debug/GenerateTestUserSig';
+import {onLaunch} from '@dcloudio/uni-app'
+
+	const globalData =  {
+		SDKAppID: genTestUserSig('').sdkAppID,
+		userID: '',
+		userSig: ''
+	};
+	onLaunch(()=> {
+		// 重点注意： 为了 uni-app 更好地接入使用 tim，快速定位和解决问题，请勿修改 uni.$TUIKit 命名。
+		// 如果您已经接入 tim ，请将 uni.tim 修改为 uni.$TUIKit。
+		uni.$TUIKit = TIM.create({
+			SDKAppID: globalData.SDKAppID
+		});
+		uni.$TIM = TIM
+	});
+</script>
