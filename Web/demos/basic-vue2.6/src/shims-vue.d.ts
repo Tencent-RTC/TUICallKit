6ae74a5a91112c00a7646aa6b677cccc2fcb152e
naturalyuan <naturalyuan@tencent.com>
2023-12-28 12:38:55 +0800

    【Web】fix: web basic demo

---
+++ Web/demos/basic-vue2.6/src/shims-vue.d.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
+
+declare module '*.svg' {
+  const content: any;
+  export default content;
+}
