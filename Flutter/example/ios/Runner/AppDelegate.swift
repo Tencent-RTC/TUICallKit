d594b3e70289a1d08ef5e105503fd833dab33525
vincepzhang <vincepzhang@tencent.com>
2024-04-12 16:23:05 +0800

    feat:flutter example access fcm push and voip push

--- Flutter/example/ios/Runner/AppDelegate.swift
+++ Flutter/example/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import TUICallKitVoIPExtension
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    TUICallKitVoIPExtension.setCertificateID(0)
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
