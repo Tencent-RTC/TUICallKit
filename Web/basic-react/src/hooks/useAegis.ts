19af2b83e652828c34d057564df3538f94454964
natural <naturalyuan@tencent.com>
2024-07-18 11:11:26 +0800

    fix: aegis config

--- Web/basic-react/src/hooks/useAegis.ts
+++ Web/basic-react/src/hooks/useAegis.ts
@@ -2,23 +2,39 @@
 import Aegis from 'aegis-web-sdk';
 import { useContext } from 'react';
 import { UserInfoContext } from '../context';
+import { getLocalStorage, setLocalStorage } from '../utils/index';
 
 interface IAegisReportParams {
   apiName?: string;
   content?: string;
-  userID?: string;
-  SDKAppID?: number;
 }
 
 export default function useAegis() {
   const { userInfo } = useContext(UserInfoContext);
+
   const aegis = new Aegis({
     id: 'iHWefAYqhifClbANcI',
+    uin: getUIN(),
     reportApiSpeed: false,
     reportAssetSpeed: false,
     spa: false,
+    websocketHack: false,
+    pagePerformance: false,
+    webVitals: false,
+    onError: false,
+    aid: false,
+    speedSample: false,
+    repeat: 1,
+    pvUrl: '',
+    whiteListUrl: '',
+    offlineUrl: '',
+    speedUrl: '',
+    webVitalsUrl: '',
+    api: {
+      apiDetail: false,
+    },
   });
-
+  
   const reportEvent = (params: IAegisReportParams) => {
     const { apiName, content } = params;
     aegis.report({ 
@@ -36,6 +52,13 @@ export default function useAegis() {
       ext1: userInfo?.SDKAppID,
       ext2: content,
      });
+  }
+  
+  function getUIN() {
+    if (!getLocalStorage('call-uikit')) {
+      setLocalStorage('call-uikit', JSON.stringify(window?.performance.now()));
+    }
+    return getLocalStorage('call-uikit') || '';
   }
 
   return {
